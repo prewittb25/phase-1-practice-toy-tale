5e4f5b240e3ebcd378077b80b634985e48310766
Barry <Prewittb25@gmail.com>
2023-01-21 01:25:22 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
-let addToy = false;
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
-});
+fetch('http://localhost:3000/toys')
+.then(response => response.json())
+.then(data => renderToys(data))
+
+function renderToys(toys) {
+  toys.forEach((toy) => {
+    const toyCollection = document.querySelector('#toy-collection')
+    const card = document.createElement('div')
+    console.log(toyCollection)
+    
+    card.className = 'card'
+    toyCollection.appendChild(card)
+    
+    const h2 = document.createElement('h2')
+    h2.textContent = toy.name
+    
+    const img = document.createElement('img')
+    img.src = toy.image
+    img.className = 'toy-avatar'
+
+    card.append(h2, img)
+    toyCollection.appendChild(card)
+
+    const p = document.createElement('p')
+    p.textContent = toy.likes
+    console.log(card)
+    card.appendChild(p)
+
+    const button = document.createElement('button')
+    button.className = 'like-button'
+    button.id = toy.id
+    button.textContent = 'Like'
+    card.appendChild(button)
+
+    card.appendChild(h2)
+    
+  
+  })
+}
+
+
+
+    
+
+
+
+   
